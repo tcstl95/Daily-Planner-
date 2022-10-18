caf8af2cd892670d2743e17896b88c2745d0075b
tcstl95 <tomcarlson171@gmail.com>
2022-10-17 23:25:32 -0500

    Commit to add ReadMe, screenshot, and gitmodules

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Daily-Planner-
+
+# Description
+
+The following Daily Planner can be used by anyone who is wanting to organize their daily schedule by the hour.
+
+Whenver a user submits an event, the event gets saved into the local storage and stays there, even if the user refreshes the page.
+
+The planner also states at the top of the page listing the current date, so the user knows the date they are planning their day around. 
+
+# Screenshot
+
+<img width="950" alt="Planner Screenshot" src="https://user-images.githubusercontent.com/107820740/196334434-c45785df-3649-4dd6-b165-6904422be1a5.png">
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,10 @@
+// Variable to set current date on Planner//
+
 var currentDate = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentDate);
+
+// Variables in order to be used for jquery event listener//
+
 var submit9amEl = $('#submit9am');
 var submit10amEl = $('#submit10am');
 var submit11amEl = $('#submit11am');
@@ -12,20 +17,7 @@ var submit5pmEl = $('#submit5pm');
 
 
 
-function saveData(){
-    localStorage.setItem('9am', 'My 9am Event');
-    localStorage.setItem('10am', 'My 10am Event');
-    localStorage.setItem('11am', "My 11am Event");
-    localStorage.setItem('12pm', "My 12pm Event");
-    localStorage.setItem('1pm', "My 1pm Event");
-    localStorage.setItem('2pm', "My 2pm Event");
-    localStorage.setItem('3pm', "My 3pm Event");
-    localStorage.setItem('4pm', "My 4pm Event");
-    localStorage.setItem('5pm', "My 5pm Event");
-
-}
-saveData()
-
+// Functions in order to return value into the local storage//
 function retrive9am(){
     return localStorage.getItem('9am');
 }
@@ -53,6 +45,8 @@ function retrive4pm(){
 function retrive5pm(){
     return localStorage.getItem('5pm');
 }
+
+// Calling functions//
 retrive9am()
 retrive10am()
 retrive11am()
@@ -63,41 +57,50 @@ retrive3pm()
 retrive4pm()
 retrive5pm()
 
-
+// Event listeners in order for user to submit events into local storage //
 submit9amEl.on('click', function(){
-    console.log(retrive9am());
+    var text = $("#Nine").val()
+    localStorage.setItem('9am', text);
     alert('You did it brother! Event Saved to local storage');
 });
 submit10amEl.on('click', function (){
-    console.log(retrive10am());
-    alert('You did it brother! Event Saved to local storage');
+    var text = $("#Ten").val()
+    localStorage.setItem('10am', text);
+   alert('You did it brother! Event Saved to local storage');
 });
 submit11amEl.on('click', function(){
-    console.log(retrive11am());
+    var text = $("#Eleven").val()
+    localStorage.setItem('11am', text);
     alert('You did it brother! Event Saved to local storage');
 });
 submit12pmEl.on('click', function(){
-    console.log(retrive12pm());
+    var text = $("#Twelve").val()
+    localStorage.setItem('12pm', text);
     alert('You did it brother! Event Saved to local storage');
 })
 submit1pmEl.on('click', function(){
-    console.log(retrive1pm());
+    var text = $("#One").val()
+    localStorage.setItem('1pm', text);
     alert('You did it brother! Event Saved to local storage');
 })
 submit2pmEl.on('click', function(){
-    console.log(retrive2pm());
+    var text = $("#Two").val()
+    localStorage.setItem('2pm', text);
     alert('You did it brother! Event Saved to local storage');
 })
 submit3pmEl.on('click', function(){
-    console.log(retrive3pm());
+    var text = $("#Three").val()
+    localStorage.setItem('3pm', text);
     alert('You did it brother! Event Saved to local storage');
 })
 submit4pmEl.on('click', function(){
-    console.log(retrive4pm());
+    var text = $("#Four").val()
+    localStorage.setItem('4pm', text);
     alert('You did it brother! Event Saved to local storage');
 })
 submit5pmEl.on('click', function(){
-    console.log(retrive5pm());
+    var text = $("#Five").val()
+    localStorage.setItem('5pm', text);
     alert('You did it brother! Event Saved to local storage');
 })
 
